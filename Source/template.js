8482f4f28d427ebefaa72c7cae2497a5aa18b49e
Denjilou <denjilouu@yahoo.com>
2014-10-15 18:32:14 -0800

    Hide And Unhide Div
    Panginoo

---
+++ Source/template.js
@@ -0,0 +1,7 @@
+function Overlay(){
+	document.getElementById("content5").style.display = "block"
+	document.getElementById("content1").style.display = "none"
+	document.getElementById("content2").style.display = "none"
+	document.getElementById("content3").style.display = "none"
+	document.getElementById("content4").style.display = "none"
+}
