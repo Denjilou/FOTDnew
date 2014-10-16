e7caa54c2918749af697d2ee1ee9f84b885fff1a
anapaolaapostol <apostol.anapaola14@gmail.com>
2014-10-16 17:20:35 +0800

    top5-div

---
+++ Source/top5.js
@@ -0,0 +1,151 @@
+function changeBfast(){
+	document.getElementById("content1").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Lugaw.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content1").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content2").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Pancakes.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content2").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content3").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Tapsilog.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content3").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content4").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Pares.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content4").appendChild(img);
+	console.log(img);
+}
+
+function changeLunch(){
+	document.getElementById("content1").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Sisig.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content1").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content2").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Steak.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content2").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content3").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/BBQ.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content3").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content4").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Crispypata.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content4").appendChild(img);
+	console.log(img);
+}
+
+function changeDrinks(){
+	document.getElementById("content1").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Icedtea.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content1").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content2").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Milkshake.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content2").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content3").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Milktea.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content3").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content4").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Halohalo.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content4").appendChild(img);
+	console.log(img);
+}
+
+function changeSnacks(){
+	document.getElementById("content1").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Burger.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content1").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content2").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Pancit.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content2").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content3").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Cupcake.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content3").appendChild(img);
+	console.log(img);
+	
+	document.getElementById("content4").innerHTML = "";
+	var img = document.createElement("img");
+	link = "resources/images/c/Halohalo.png";
+	img.setAttribute("src",link);
+	img.setAttribute("height","280px");
+	img.setAttribute("width","450px");
+	document.getElementById("content4").appendChild(img);
+	console.log(img);
+}
