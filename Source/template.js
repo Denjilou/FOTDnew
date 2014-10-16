4f0d33bb33db52146cdaa3ff02e6362755682e21
Denjilou <denjilouu@yahoo.com>
2014-10-15 18:42:59 -0800

    Close Div

--- Source/template.js
+++ Source/template.js
@@ -5,3 +5,10 @@ function Overlay(){
 	document.getElementById("content3").style.display = "none"
 	document.getElementById("content4").style.display = "none"
 }
+function Restore(){
+	document.getElementById("content5").style.display = "none"
+	document.getElementById("content1").style.display = "block"
+	document.getElementById("content2").style.display = "block"
+	document.getElementById("content3").style.display = "block"
+	document.getElementById("content4").style.display = "block"
+}
