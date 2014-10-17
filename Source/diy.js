1ee5e4d66f3aa7fd908c358b77237c3c4c39b572
rogh <hpyvrsyeol@gmail.com>
2014-10-17 11:27:54 -0700

    cooking manong

--- Source/diy.js
+++ Source/diy.js
@@ -17,3 +17,30 @@ kaldereta.ing = ["mantika","patatas","liverspread","beef","tubig","sili","cheese
 karekare.ing = ["beef","tubig","sitaw","pechay","talong","sibuyas","bawang","peanutbutter","alamang"];
 tinola.ing=["manok","malunggay","sayote","tubig","mantika","luya","bawang","sibuyas"];
 pinakbet.ing=["baboy","bawang","kamatis","asin","luya","paminta","sibuyas","tubig","kalabasa","alamang","okra","sitaw","talong",];
+
+var cm = document.createElement("img");
+cm.setAttribute("id","manong");
+cm.setAttribute("src", "resources/image/ManongCook2.png");
+document.getElementById("content5").appendChild(cm);
+
+var p = document.createElement("p");
+p.setAttribute("id","cm");
+document.getElementById("content5").appendChild(p);
+document.getElementById("cm").innerHTML = "Welcome to Cooking Manong!";
+
+var p = document.createElement("p");
+p.setAttribute("id","con");
+document.getElementById("content5").appendChild(p);
+document.getElementById("con").innerHTML = "This Module will virtually help you cook your meals.<br/> To begin please choose your meal category.";
+		
+var meal = document.createElement("p");
+meal.setAttribute("id","a");
+meal.setAttribute("class","meal1");
+document.getElementById("content5").appendChild(meal);
+document.getElementById("a").innerHTML = "Meal";
+
+var meal1 = document.createElement("p");
+meal1.setAttribute("id","b");
+meal1.setAttribute("class","meal1");
+document.getElementById("content5").appendChild(meal1);
+document.getElementById("b").innerHTML = "Snacks";
